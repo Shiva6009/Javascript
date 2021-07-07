7f4889db65cc7f764137241377cfd46ec6b92eb5
Shiva6009 <57894709+Shiva6009@users.noreply.github.com>
2021-07-07 09:38:53 +0530

    Delete ProblemUC1.js

--- ProblemUC1.js
+++
@@ -1,7 +0,0 @@
- createtictacarray();
-    function createtictacarray()
-    {
-        var tictacarray =[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '];
-        var updatedtictacarray = tictacarray.slice(1,10);
-        console.log(" Array Created with Length.." , updatedtictacarray.length);
-    }
